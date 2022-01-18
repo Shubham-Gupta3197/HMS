8051a6bf9e3ee6a77f4eec612f43e313b935299d
vikas159 <shubhamguptaclan@gmail.com>
2022-01-18 22:49:08 +0530

    Intial

---
+++ src/hotel_management_system/Hotel_Management_System.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotel_management_system;
+
+/**
+ *
+ * @author Akshit Chaudhary
+ */
+public class Hotel_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          HotelManagementSystem window = new HotelManagementSystem();
+                window.setVisible(true);
+    }
+    
+}
